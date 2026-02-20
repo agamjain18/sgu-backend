86572401f75a8bcdd1b96b0bb68d4969f63295a7
agamjain18 <143345406+agamjain18@users.noreply.github.com>
2026-02-21 00:29:37 +0530

    fix: api white screen issue by fixing CORS headers and path routing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "sgu-backend",
         script: "./venv/bin/python3",
-        args: "-m uvicorn main:app --host 0.0.0.0 --port 8022 --proxy-headers",
+        args: "-m uvicorn main:app --host 0.0.0.0 --port 8022 --proxy-headers --root-path /sgu",
         interpreter: "none",
         env: {
             NODE_ENV: "production",
